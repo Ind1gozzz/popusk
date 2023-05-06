509a81c23f3368b3a5f002f099130daa29361dfd
Ind1gozzz <atoyan.dawid@yandex.ru>
2023-05-06 15:26:53 +0300

    SEQUE

--- src/models.js
+++ src/models.js
@@ -1,9 +1,29 @@
 const db = require("./db")
-const { DataTypes } = require("sequelize")
+const { DataTypes, STRING } = require("sequelize")
 
 const User = db.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: "USER" }
+})
+
+const Lesson = db.define("lesson", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING },
+})
+
+const Question = db.define("question", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  text: { type: DataTypes.INTEGER, allowNull: false },
+  type: { type: DataTypes.ENUM("R", "T"), allowNull: false },
+  answer: { type: DataTypes.STRING, allowNull: false },
+  lessonId: { type: DataTypes.INTEGER, allowNull: false }
+})
+
+const UserProgress = db.define("userprogress", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  questionId: { type: DataTypes.INTEGER, allowNull: false },
+  progress: { type: DataTypes.INTEGER, defaultValue: 0 }
 })
