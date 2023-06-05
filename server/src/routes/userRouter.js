baa4738cc1bd02e5a2c0a17ab66deedf1cfea8f7
Ind1gozzz <56315894+Ind1gozzz@users.noreply.github.com>
2023-06-05 18:14:52 +0300

    auth

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -3,11 +3,28 @@ const { User } = require("../models")
 const bcrypt = require("bcrypt")
 
 const router = Router()
+
 router.post("/registration", async function (req, res) {
   const { email, password } = req.body
   const hashPass = bcrypt.hash(password, 12)
   const user = await User.create({ email, hashPass })
   return res.json(user)
+})
+
+router.post("/authorization", async function (req, req) {
+  const { email, password } = req.body
+  const userCandidate = await User.findOne({ where: { email } })
+  if (!user) {
+    return "User not found"
+  }
+  let comparePassword = bcrypt.compareSync(password, user.password)
+  if (comparePassword) {
+    const token = generateJwt(user.id, user.email)
+    return res.json(token)
+  }
+  else {
+    return "Passwords not compare"
+  }
 })
 
 module.exports = router
