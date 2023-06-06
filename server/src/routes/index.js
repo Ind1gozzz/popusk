eebfc22957ccaf69de93a37f5647a933933bfc60
Ind1gozzz <56315894+Ind1gozzz@users.noreply.github.com>
2023-06-06 18:38:05 +0300

    progress

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express")
 const router = new Router()
 const userRouter = require("./userRouter")
+const questionRouter = require("./questionRouter")
 
 router.use("/user", userRouter)
+router.use("/question", questionRouter)
 
 module.exports = router
---
+++ server/src/routes/questionRouter.js
@@ -0,0 +1,17 @@
+const { Router } = require("express")
+const Router = require("express")
+const { Question, UserProgress } = require("../models")
+
+const router = Router()
+
+router.get("/fetchQuestions", async function (req, res) {
+  const { lessonId } = req.query
+  const questions = await Question.findAll({ where: { lessonId: lessonId } })
+  return res.json(questions)
+})
+
+router.post("/updateResult", async function (req, res) {
+  const { userId, questionId, userProgress } = req.body
+  const userprogress = await UserProgress.insertOrUpdate({ progress: userProgress, userId: userId, where: { questionId } })
+  return res.json(userprogress)
+})
